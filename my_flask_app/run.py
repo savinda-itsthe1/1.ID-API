207229d2beda77fecfa023aeda0ce89de99980a4
nikethani997 <savinda.itsthe1@gmail.com>
2025-11-19 12:54:16 +0530

    production , for old database, however port not in the config file

--- my_flask_app/run.py
+++ my_flask_app/run.py
@@ -1,6 +1,11 @@
+import os
 from app import create_app
+from waitress import serve
+
+# Ensure working directory is where the exe is located
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    serve(app, host="0.0.0.0", port=5000)
